9cf4f2edc1271fce5b324cb6fe02261493b91508
Jason <jasonjayo2002@gmail.com>
2019-02-05 21:24:25 +0000

    Initial commit

---
+++ erasmus.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/erasmus.css">
+        <link rel="stylesheet" href="css/global.css">
+    </head>
+    <body>
+        <nav class="headerNav">
+            Internationalisation @ Carrick
+            <ul>
+                <li><a href="/ccs/internationalisation">Home</a></li>
+                <li><a href="erasmus.php">Erasmus+</a></li>
+                <li><a href="#">eTwinning</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">History</a></li>
+            </ul>
+        </nav>
+        <header>
+        Erasmus+
+        </header>
+        <main>
+            <section id="info">
+                <div>Erasmus+ is the European Commission's Programme for education, training, youth and sport for the period 2014–2020, succeeding the previous Lifelong Learning Programme (2007 - 2014).</div>
+                <div>As an integrated programme, Erasmus+ offers more opportunities for mobility of learners and staff and cooperation across the education, training and youth sectors and is easier to access than its predecessors, with simplified funding rules and a structure which aims to streamline the administration of the programme.</div>
+            </section>
+            <section id="links">
+                <a href="#">
+                    <div>
+                        <h2>Erasmus+ KA101</h2>
+                        <div class"linkDesc">Celebrating Education through Creativity</div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div>
+                        <h2>Erasmus+ KA219</h2>
+                        <div class"linkDesc">Celebrating Education</div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div>
+                        <h2>Erasmus+ KA101</h2>
+                        <div class"linkDesc">CarrickCS/SIC Ljubljana collaboration</div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div>
+                        <h2>Erasmus+ Visiting teachers &amp; educationalists</h2>
+                    </div>
+                </a>
+            </section>
+        </main>
+        
+    </body>
+</html>
